6fa2cecdb17ea16db9b71ced590c35800b89a20a
maxdemaio <maxdman321@gmail.com>
2024-08-23 16:08:19 -0400

    align w/ number

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,72 +39,71 @@ export default async function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center px-8 pt-8 md:pt-20 w-full">
       <div>
-      <header className="flex flex-col gap-8 pb-20">
-        <h1 className="text-4xl md:text-5xl font-bold bg-gradient-to-r from-pink-400 to-orange-400 dark:from-pink-300 dark:to-orange-300 bg-clip-text text-transparent">
-          Product Hunt with ZERO AI Slop™
-        </h1>
-        <div className="flex flex-col gap-2">
-          <div className="text-3xl font-bold">{`${posts.length} products without AI launched today`}</div>
-          <div className="text-xl">
-            {aiPosts.length} AI Slop™ projects launched today
+        <header className="flex flex-col gap-8 pb-20 px-4 md:px-8">
+          <h1 className="text-4xl md:text-5xl font-bold bg-gradient-to-r from-pink-400 to-orange-400 dark:from-pink-300 dark:to-orange-300 bg-clip-text text-transparent">
+            Product Hunt with ZERO AI Slop™
+          </h1>
+          <div className="flex flex-col gap-2">
+            <div className="text-3xl font-bold">{`${posts.length} products without AI launched today`}</div>
+            <div className="text-xl">
+              {aiPosts.length} AI Slop™ projects launched today
+            </div>
+          </div>
+        </header>
+
+        <div>
+          <div className="flex flex-col gap-8 overflow-hidden">
+            {posts.map((post, index) => {
+              return (
+                <a
+                  href={post.url}
+                  key={post.id}
+                  target="_blank"
+                  className="flex flex-col md:flex-row items-center gap-8 p-4 md:p-8 group hover:bg-neutral-300/50 dark:hover:bg-neutral-900 rounded-2xl duration-300 cursor-pointer"
+                >
+                  <div className="hidden md:flex flex-row items-center justify-center pb-2 gap-4">
+                    <div className="border rounded-lg p-4 border-neutral-700 text-xl">
+                      #{index + 1}
+                    </div>
+                  </div>
+
+                  {post.thumbnailUrl && (
+                    <Image
+                      src={post.thumbnailUrl}
+                      height={100}
+                      width={100}
+                      className="rounded-lg"
+                      alt="logo"
+                    />
+                  )}
+                  <div className="flex flex-col items-start gap-2">
+                    <h2 className="text-2xl line-clamp-3 max-w-[69ch] md:text-4xl font-bold group-hover:underline duration-300 group-hover:translate-x-2">
+                      {post.name}
+                    </h2>
+                    <p className="text-base md:text-lg max-w-[69ch] opacity-60">
+                      {post.tagline}
+                    </p>
+                    <div className="flex flex-wrap gap-2">
+                      {post.topics &&
+                        post.topics.map(({ id, name }) => (
+                          <Pill key={`${id}${post.id}`} name={name} />
+                        ))}
+                    </div>
+                    <p className="line-clamp-3 text-base md:text-lg max-w-[69ch]">
+                      {post.description}
+                    </p>
+                  </div>
+
+                  <div className="hidden md:flex ml-auto flex-col items-center border border-neutral-700 rounded-lg px-4 py-2">
+                    <UpArrow className="stroke-0 h-12 w-12" gradient />
+                    <p className="font-bold">{post.votesCount}</p>
+                  </div>
+                </a>
+              );
+            })}
           </div>
         </div>
-      </header>
-
-      <div>
-        <div className="flex flex-col gap-8 overflow-hidden">
-          {posts.map((post, index) => {
-            return (
-              <a
-                href={post.url}
-                key={post.id}
-                target="_blank"
-                className="flex flex-col md:flex-row items-center gap-8 p-4 md:p-8 group hover:bg-neutral-300/50 dark:hover:bg-neutral-900 rounded-2xl duration-300 cursor-pointer"
-              >
-                <div className="hidden md:flex flex-row items-center justify-center pb-2 gap-4">
-                  <div className="border rounded-lg p-4 border-neutral-700 text-xl">
-                    #{index + 1}
-                  </div>
-                </div>
-
-                {post.thumbnailUrl && (
-                  <Image
-                    src={post.thumbnailUrl}
-                    height={100}
-                    width={100}
-                    className="rounded-lg"
-                    alt="logo"
-                  />
-                )}
-                <div className="flex flex-col items-start gap-2">
-                  <h2 className="text-2xl line-clamp-3 max-w-[69ch] md:text-4xl font-bold group-hover:underline duration-300 group-hover:translate-x-2">
-                    {post.name}
-                  </h2>
-                  <p className="text-base md:text-lg max-w-[69ch] opacity-60">
-                    {post.tagline}
-                  </p>
-                  <div className="flex flex-wrap gap-2">
-                    {post.topics &&
-                      post.topics.map(({ id, name }) => (
-                        <Pill key={`${id}${post.id}`} name={name} />
-                      ))}
-                  </div>
-                  <p className="line-clamp-3 text-base md:text-lg max-w-[69ch]">
-                    {post.description}
-                  </p>
-                </div>
-
-                <div className="hidden md:flex ml-auto flex-col items-center border border-neutral-700 rounded-lg px-4 py-2">
-                  <UpArrow className="stroke-0 h-12 w-12" gradient />
-                  <p className="font-bold">{post.votesCount}</p>
-                </div>
-              </a>
-            );
-          })}
-        </div>
       </div>
-      </div>
-      
 
       <Analytics />
     </main>
